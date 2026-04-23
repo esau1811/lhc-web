edc738ddcea287fd2af3d09475a1025b45d6e2fa
Dev <dev@ejemplo.com>
2026-04-23 19:07:08 +0200

    Actualizados iconos de Discord (logo oficial) y Banner (Copa) según las capturas de referencia

--- src/app/page.js
+++ src/app/page.js
@@ -215,8 +215,10 @@ export default function HomePage() {
 
             <Link href="https://discord.gg/AS46Hlp2vO" className="flex items-center justify-between bg-[#5865F2] hover:bg-[#4752c4] p-4 rounded-2xl transition-all group relative">
               <div className="flex items-center gap-3 relative z-10">
-                <div className="bg-white/20 p-2 rounded-lg">
-                  <MessageSquare size={18} className="text-white" />
+                <div className="p-1">
+                  <svg width="32" height="32" viewBox="0 0 24 24" fill="white">
+                    <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.419c0 1.334-.947 2.419-2.157 2.419zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.419c0 1.334-.946 2.419-2.157 2.419z"/>
+                  </svg>
                 </div>
                 <div>
                   <p className="text-[11px] font-black uppercase leading-none text-white">Únete a Discord</p>
@@ -327,8 +329,8 @@ export default function HomePage() {
             {/* BOTTOM BANNER */}
             <div className="mt-8 relative overflow-hidden rounded-3xl bg-gradient-to-r from-yellow-500/10 to-transparent border border-yellow-500/10 p-8 flex items-center justify-between group">
               <div className="flex items-center gap-6">
-                <div className="bg-yellow-500 w-16 h-16 rounded-2xl flex items-center justify-center text-black shadow-[0_0_20px_rgba(234,179,8,0.3)]">
-                  <User size={32} />
+                <div className="bg-yellow-500/5 p-4 rounded-2xl text-yellow-500 border border-yellow-500/20 shadow-[0_0_30px_rgba(234,179,8,0.1)]">
+                  <Trophy size={32} />
                 </div>
                 <div>
                   <h4 className="text-xl font-black uppercase tracking-tight leading-none mb-2">Únete a miles de jugadores</h4>
