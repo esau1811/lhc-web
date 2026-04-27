1ed2eaeb66cbae61e196b2a7042dffc09db745fd
Dev <dev@ejemplo.com>
2026-04-27 10:23:27 +0200

    style: increase logo size in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ export default function Header() {
           
           <div className="flex items-center gap-8">
             <Link href="/" className="flex items-center gap-2">
-              <img src="/logo.png" alt="LHC" className="h-8 w-auto" />
+              <img src="/logo.png" alt="LHC" className="h-12 w-auto" />
             </Link>
 
             {/* Language Selector Desktop */}
